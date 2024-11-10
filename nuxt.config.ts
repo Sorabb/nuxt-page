45864c2fcceb184f17ed2f509196832539bd0d76
sora <dongnaitian@gmail.com>
2024-11-10 19:00:43 +0800

    调整静态文件目录

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    buildAssetsDir: '/static/',
+  },
   devtools: { enabled: true },
   devServer: {
     port: 11345,
